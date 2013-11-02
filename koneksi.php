6db4c3101d8e9efa922dd3bb509def64b9e6da4e
Nydan <Nydan@users.noreply.github.com>
2013-11-02 14:29:24 +0700

    Create koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,7 @@
+<?php
+    $db_user = "readmeshop";
+    $db_password = "5hAHfL6GFwzKqymu";
+    $db_host = "localhost";
+    $db_database = "readmeshop";
+    $koneksi = mysqli_connect($db_host,$db_user,$db_password,$db_database);
+?>
