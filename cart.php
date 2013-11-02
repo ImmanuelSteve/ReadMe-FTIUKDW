0016a96bcc694f5c32f5978fa1999c7be96454a3
Nydan <Nydan@users.noreply.github.com>
2013-11-02 14:27:02 +0700

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML  >
+    <html>
+        <head>
+            <title>ReadMe Shop - Cart</title>
+            <link rel="stylesheet" href="css/960_24_col.css" />
+            <link rel="stylesheet" href="css/style.css" />
+            <link rel="stylesheet" href="css/cart.css" />
+            <link href="css/js-image-slider.css" rel="stylesheet" type="text/css" />
+        </head>
+        
+        <body>
+            
+        </body>
+    </html>
