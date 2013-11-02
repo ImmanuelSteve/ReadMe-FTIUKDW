b830a1b1fe473ededb4bd18cc4ba57dddfcc5e3b
Nydan <Nydan@users.noreply.github.com>
2013-11-02 14:31:12 +0700

    Update cart.php

--- cart.php
+++ cart.php
@@ -9,6 +9,46 @@
         </head>
         
         <body>
+            <div id="header">
+        <div class="container_24">
+            <div class="grid_4">
+                <img src="images/readmeshoplogo.png" height="100" width="110"/>
+            </div>
+            <div id="login" class="grid_20">
+                <form id="formlogin">
+                    <input type="text" name="username" placeholder="nama pengguna">
+                    <input type="password" name="password" placeholder="kata sandi">
+                    <button type="submit">Masuk</button>
+                     <br/>Belum punya akun <a href="daftar.html">daftar disini</a>
+                </form>
+            </div>
             
+            <div class="clear"></div>
+            
+            <div class="grid_24" id="header_nav">
+                <ul id="nav">
+                    <li><a class="navi" href="index.php">Beranda</a> </li>
+                    <li><a class="navi" href="produk.html">Produk </a></li>
+                    <li><a class="navi" href="promosi.php">Promosi </a></li>
+                    <li><a class="navi" href="tentangkami.html">Tentang kami </a></li>
+                </ul>
+                <div id="formsearch" class="grid_6">
+                    <form>
+                        <div id="textsearch" class="grid_4">
+                            <input type="text" name="pencarian" placeholder="Pencarian">
+                        </div>
+                        <div id="buttonsearch" class="grid_1">
+                            <input type="image" src="images/search.png" name="submit">
+                        </div>
+                        <!--<button class"buttonsubmit" type="submit">Cari</button>-->
+                    </form>
+                </div>
+            </div>
+            
+            <div class="clear"></div>
+        
+        </div>
+    </div>  
         </body>
     </html>
+    
