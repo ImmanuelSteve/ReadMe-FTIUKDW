77a109bf9414c3d4a7ebe62e88cde42502b06826
Nydan <Nydan@users.noreply.github.com>
2013-11-05 20:32:27 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,7 @@
 <?php
-    $db_user = "readmeshop";
+    $db_user = "readmesh_readsh";
     $db_password = "5hAHfL6GFwzKqymu";
     $db_host = "localhost";
-    $db_database = "readmeshop";
+    $db_database = "readmesh_readmeshop";
     $koneksi = mysqli_connect($db_host,$db_user,$db_password,$db_database);
 ?>
