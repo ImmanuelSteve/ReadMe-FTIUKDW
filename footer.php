5a304c2c1bb1609f31cff235c09908d3803592c3
71110054 <steve.immannuel@gmail.com>
2013-11-16 23:28:50 +0700

    pisahkan footer jd 1file

---
+++ footer.php
@@ -0,0 +1,68 @@
+<div id="footer">
+        <div class="container_24">
+            <div class="grid_24" id="footerarea">
+                <div class="grid_4 footerleft">
+		    <p class="textfootertitle center">Merek Kami</p>
+		    <ul class="center hover">
+			<li><a class="textfooterinfo" href="#">Apple</a></li>
+                        <li><a class="textfooterinfo" href="#">BlackBerry</a></li>
+                        <li><a class="textfooterinfo" href="#">HTC</a></li>
+                        <li><a class="textfooterinfo" href="#">Huawei</a></li>
+                        <li><a class="textfooterinfo" href="#">Lenovo</a></li>
+                        <li><a class="textfooterinfo" href="#">LG</a></li>
+                        <li><a class="textfooterinfo" href="#">Nokia</a></li>
+                        <li><a class="textfooterinfo" href="#">Samsung</a></li>
+                        <li><a class="textfooterinfo" href="#">Sony</a></li>
+                        <li><a class="textfooterinfo" href="#">ZTE</a></li>
+                    </ul>
+		</div>
+                <div class="grid_4">
+		    <p class="textfootertitle center">Link</p>
+		    <ul class="center hover">
+			<li><a class="textfooterinfo" href="produk.php">Produk</a></li>
+                        <li><a class="textfooterinfo" href="#">Produk Baru</a></li>
+                        <li><a class="textfooterinfo" href="#">Produk Terlaris</a></li>
+                        <li><a class="textfooterinfo" href="promosi.php">Promosi</a></li>
+                    </ul>
+		</div>
+                <div class="grid_4">
+		    <p class="textfootertitle center">Gabung Yuk</p>
+		    <ul class="center hover">
+			<li><a class="textfooterinfo textfooterbig" href="http://www.facebook.com" target="blank">Facebook</a></li>
+                        <li><a class="textfooterinfo textfooterbig" href="http://www.instagram.com" target="blank">Instagram</a></li>
+                        <li><a class="textfooterinfo textfooterbig" href="http://www.twitter.com" target="blank">Twitter</a></li>
+                    </ul>
+		</div>
+                 <div class="grid_6">
+		    <p class="textfootertitle center">HOTLINE</p>
+		    <ul class="center">
+			<li class="texthotline textfooterbig">Contact us :</li>
+                        <li class="texthotline textfooterbig">+62 81 234 567 89</li>
+                         <li class="texthotline">Untuk informasi lebih lanjut</li>
+                    </ul>
+		</div>
+                 <div class="grid_5">
+		    <p class="textfootertitle center">Jasa Pengiriman</p>
+		    <ul class="center hover">
+			<li><a class="textfooterinfo" href="http://www.jne.co.id" target="blank">
+                            <img src="images/JNE.png">
+                        </a></li>
+                        <li><a class="textfooterinfo" href="http://www.posindonesia.co.id" target="blank">
+                            <img src="images/POS.jpg">
+                        </a></li>
+                        <li><a class="textfooterinfo" href="http://www.tiki-online.com" target="blank">
+                            <img src="images/TIKI.png">
+                        </a></li>
+                    </ul>
+		</div>
+            </div>
+            <div class="grid_24" id="copyright">
+                <p id="copyrighttext" class="center">
+                    Copyright @2013 ReadmeShop All Right Reserved
+                </p>
+            </div>
+            
+            <div class="clear"></div>
+        
+        </div>
+</div>
