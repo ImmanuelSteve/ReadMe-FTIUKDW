1d819bcae6d06c33e189e6f4b87792a3d0aaeca9
71110054 <steve.immannuel@gmail.com>
2013-11-21 19:42:46 +0700

    compare.php tp blm jadi baru template

---
+++ compare.php
@@ -0,0 +1,96 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="Description" content="readmeshop.com, gadget, smartphone, toko online, online shop, toko gadget"/>
+    <title>Bandingkan Produk - ReadMe shop</title>
+    <link rel="icon" type="image/png" href="images/icon-readmeshop.png" />
+    <link rel="stylesheet" href="css/960_24_col.css" />
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/compare.css" />
+    <script type="text/javascript" src="js/jquery-2.0.3.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="js/jquery.lavalamp.min.js"></script>
+    <script type="text/javascript" src="js/lamp.js"></script>
+</head>
+
+<body>
+    <!-- header begin -->
+    <div id="header">
+        <div class="container_24">
+            <div class="grid_4">
+                <img src="images/readmeshoplogo.png" height="100" width="110"/>
+            </div>
+            <!--login-->
+            <?php
+                $_SESSION['actionlogin'] = "compare.php";
+                include("login.php");
+            ?>
+            <div class="grid_24" id="header_nav">
+                <ul class="lavaLamp">
+                    <li class="current"><a href="index.php">Beranda</a> </li>
+                    <li><a href="produk.php">Produk </a></li>
+                    <li><a href="promosi.php">Promosi </a></li>
+                    <li><a href="tentangkami.php">Tentang kami </a></li>
+                </ul>
+                <div id="formsearch" class="grid_6">
+                    <form accept-charset="utf-8" method="post" action="cari.php">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <input id="textsearch" type="search" required x-moz-errormessage="Inputan jangan kosong !" size="22" value="" name="search" placeholder="cari produk"></input>
+                                </td>
+                                <td>
+                                    <input id="buttonsearch" type="submit" style="cursor:pointer;" value="" name=""></input>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </form>
+                </div>
+            </div>
+            
+            <div class="clear"></div>
+        
+        </div>
+    </div>
+    <!-- header end -->
+    
+    <!-- content begin -->
+    <div id="content">
+        <div class="container_24">
+            <div id="contentarea" class="grid_24">
+                <h1>Bandingkan Produk</h1>          
+                <div class="grid_24">
+                    <label>Produk 1 : </label>
+                    <select name='warna'>
+                        <option value='1'>Samsung</option>
+                        <option value='2'>Sony</option>
+                        <option value='3'>Huawei</option>
+                    </select>
+                    <label id="labelproduk2">Produk 2 : </label>
+                    <select name='warna'>
+                        <option value='1'>Samsung</option>
+                        <option value='2'>Sony</option>
+                        <option value='3'>Huawei</option>
+                    </select>
+                    <input id="buttonbandingkan" type="submit" name="compare" value="Bandingkan">
+                </div>
+            </div>
+           
+        </div>
+    </div>
+    <!-- content end -->
+
+    <!-- footer begin -->
+    <?php
+        require("footer.php");
+    ?>
+    <!-- footer end -->
+
+</body>
+</html>
